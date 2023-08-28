4ec2f01d0c8cced82f8927a123c57a74c4a6546e
422dsolomon <43157672+422dsolomon@users.noreply.github.com>
2023-08-28 14:03:16 -0700

    added animations

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,8 @@ import { useState, useEffect} from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons"
 import headerImg from "../assets/img/header-img.svg"
+import 'animate.css';
+import TrackVisibility from "react-on-screen";
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
@@ -44,10 +46,15 @@ export const Banner = () => {
             <Container>
                 <Row className="align-items-center">
                     <Col xs = {12} md = {6} xl ={7}>
-                        <span className="tagline">Welctom to My Portfolio</span>
-                        <h1>{`Hi I'm Danny `}<span className="wrap">{text}</span></h1>
-                        <p>My name is Danny Solomon. I am a recent 2023 graduate from Harvard College with a concentration in biomedical engineering. I am passionate about the intersection of health and technology, aiming to utilize innovative technological solutions to advance healthcare. I have proficiency in software engineering, cell culturing, experimental design and digital fabrication, enabling me to tackle complex projects effectively. One of my achievements includes the development of an artificial nose for biosensing, showcasing my ability to translate my skill set into tangible results. As a dedicated student-athlete throughout my four years in college, I have honed my communication, leadership, and teamwork skills. These skills and experiences merge seamlessly with my passion for healthcare technology, allowing me to contribute effectively within this dynamic field.</p>
-                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size = {25}/></button>
+                        <TrackVisibility>
+                        {({ isVisible }) =>
+                            <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                                <span className="tagline">Welctom to My Portfolio</span>
+                                <h1>{`Hi I'm Danny `}<span className="wrap">{text}</span></h1>
+                                <p>My name is Danny Solomon. I am a recent 2023 graduate from Harvard College with a concentration in biomedical engineering. I am passionate about the intersection of health and technology, aiming to utilize innovative technological solutions to advance healthcare. I have proficiency in software engineering, cell culturing, experimental design and digital fabrication, enabling me to tackle complex projects effectively. One of my achievements includes the development of an artificial nose for biosensing, showcasing my ability to translate my skill set into tangible results. As a dedicated student-athlete throughout my four years in college, I have honed my communication, leadership, and teamwork skills. These skills and experiences merge seamlessly with my passion for healthcare technology, allowing me to contribute effectively within this dynamic field.</p>
+                                <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size = {25}/></button>
+                            </div>}
+                        </TrackVisibility>
                     </Col>
                     <Col cs = {12} md ={6} xl = {5}>
                         <img src = {headerImg} alt = 'Header Img'></img>
