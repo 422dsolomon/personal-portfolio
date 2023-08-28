b3764a0fdc7254b83d3a3a5bc71224aa9e1ff538
422dsolomon <43157672+422dsolomon@users.noreply.github.com>
2023-08-28 13:07:22 -0700

    added project cards and project tabs

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,10 +1,10 @@
 import { Col } from "react-bootstrap";
-export const ProjectCard = ({title, description, imgUrl}) => {
+export const ProjectCard = ({title, description, imgURL}) => {
     return (
         <Col  sm= {6} md = {4}>
             <div className="proj-imgbx">
-                <img src={imgUrl} alt="proj_img"/>
-                <div className="proj-txt">
+                <img src={imgURL} alt = "proj_img"/>
+                <div className="proj-txtx">
                     <h4>{title}</h4>
                     <span>{description}</span>
                 </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,9 @@
 import { Container, Row, Col, Nav, Tab} from "react-bootstrap";
 import { ProjectCard } from "./ProjectCards";
 import colorSharp2 from "../assets/img/color-sharp2.png"
-import projImg1 from "../assets/img/project-img1.png"
+import projImg1 from '../assets/img/project-img1.png'
+import projImg2 from "../assets/img/project-img2.png"
+import projImg3 from "../assets/img/project-img3.png"
 
 export const Projects = () => {
 
@@ -10,6 +12,16 @@ export const Projects = () => {
             title: "business startup",
             description: " Design & Development",
             imgURL: projImg1,
+        },
+        {
+            title: "business startup",
+            description: " Design & Development",
+            imgURL: projImg2,
+        },
+        {
+            title: "business startup",
+            description: " Design & Development",
+            imgURL: projImg3 ,
         },
     ];
     return (
@@ -20,7 +32,7 @@ export const Projects = () => {
                     <h2>Projects</h2>
                     <p>Need to fill in</p>
                     <Tab.Container id="projects-tabs" defaultActiveKey = "first">
-                        <Nav variant = "pills" defaultActiveKey= "/home">
+                        <Nav variant = "pills" className="nav-pills mb-5 justify-content-center align-items-center" id = "pills-tab">
                             <Nav.Item>
                                 <Nav.Link eventKey ="first">Tab One</Nav.Link>
                             </Nav.Item>
